88d77939eb3ea85f68574094e436fa62f57d2f04
Nikhil Chinchalkar <147773549+nikhilc52@users.noreply.github.com>
2024-07-25 23:47:43 -0400

    Update tree_analysis.R

--- other_resources/tree_analysis.R
+++ other_resources/tree_analysis.R
@@ -1,7 +1,9 @@
 library(tidyverse)
 
+#read in csv of the tree census
 df <- read_csv("2015_Street_Tree_Census_-_Tree_Data_20240718.csv")
 
+#parse data and summarize to find given variables
 data <- df |> 
   filter(borough%in%c("Manhattan","Bronx")) |> 
   group_by(nta_name) |> 
